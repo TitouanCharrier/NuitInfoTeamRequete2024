ddcd456e19b9cec02b51ebc84f37875bdaae1af3
Tommy-Verdi Oravec <oravec.tv@etud.univ-pau.fr>
2024-12-06 06:58:01 +0100

    final commmergeit mario

--- ts/main.ts
+++ ts/main.ts
@@ -6,11 +6,8 @@ let gametcha = new Gametcha((res) => {console.log(res)});
 
 gametcha.show();
 
-<<<<<<< HEAD
 let game: GameWindow = new GameWindow(gametcha);
 
-=======
 let bh = new BulletHell(gametcha);
 
 bh.start();
->>>>>>> gametcha
